66c18d419f1ca102ba753a7e5ab5208d8f64a060
Thrinity <1440401918@qq.com>
2019-04-03 19:03:02 +0800

    增加大数（超过普通数据类型的表示范围），重载了加，减，乘。 没有实现除，目前对于除的想法就是通过乘法达到近似，但是这样做意义不大。 不得不说大数的运算还是很有趣的

--- Projects/BigInt/Main.cpp
+++ Projects/BigInt/Main.cpp
@@ -2,16 +2,48 @@
 //
 
 #include "stdafx.h"
-
+#include <string>
 #include "BigInt.h"
 
 int main(int argc, char* argv[])
 {
-	BigInt a( "1234567890" );
-	BigInt b( "1000000000" );
-	BigInt c = a * b;
-	std::cout << c << std::endl;
 
+	char op;
+	cout<<"which operation you want to choose?('+','-','*')"<<endl;
+	op=getchar();
+	
+	string str1,str2;
+	cout<<"please input two big number"<<endl;
+	cin>>str1;
+	getchar();
+	cin>>str2;
+	cout<<"a="<<str1<<"  "<<"b="<<str2<<endl;
+
+
+	BigInt a( str1 );
+	BigInt b( str2 );
+	BigInt c;
+	switch(op){
+	case '+':
+		c = a + b;
+		std::cout <<"a+b="<<c<< std::endl;
+		break;
+	case '-':
+		c = a - b;
+		std::cout <<"a-b="<<c<< std::endl;
+		break;
+	case '*':
+		c = a * b;
+		std::cout <<"a*b="<<c<< std::endl;
+		break;
+	default:
+		std::cout <<"can't run!"<< std::endl;
+	}
+
+	//BigInt a( "1234567890" );
+	//BigInt b( "1000000000" );
+
+	//std::cout << c << std::endl;
 	system( "pause" );
 
 	return 0;
